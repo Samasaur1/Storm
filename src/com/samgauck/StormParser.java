79866735d78467aa601804646c36e45cd6ccf76c
Sam <30577766+Samasaur1@users.noreply.github.com>
2017-12-12 17:03:12 -0500

    Make indicator message more descriptive

--- src/com/samgauck/StormParser.java
+++ src/com/samgauck/StormParser.java
@@ -2,7 +2,7 @@ package com.samgauck;
 
 public class StormParser {
     public static void parseLine(String line) {
-        line += "\twitten bi me"; //for now, just show it was accessed
+        line += "\t//has been processed by the parser"; //for now, just show it was accessed
 
         //Actually write the line
         Main.writeLine(line);
