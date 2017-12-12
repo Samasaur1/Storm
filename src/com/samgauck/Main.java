9b769119fb3bbc886f0b2e05df3da1e3a40856d0
Sam Gauck <21samuelg@abingtonfriends.net>
2017-12-11 19:42:54 -0500

    Add enough functionality for it to run
    Takes a file as input, and outputs a (invalid) .java file that simply adds "\twitten bi me" to show it was accessed. Needless to say, change this

--- src/com/samgauck/Main.java
+++ src/com/samgauck/Main.java
@@ -1,26 +1,49 @@
 package com.samgauck;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.net.URI;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
 
-    public static File writeFile;
+    public static PrintWriter w = null;
 
     public static void main(String[] args) {
         try {
-            Scanner s = new Scanner(System.in);
-            System.out.println("Please enter the full file path to compile: (Use ~ for your home directory)");
-            String filePathString = s.nextLine();
-            file = new File(filePathString);
-            BufferedReader b = Files.newBufferedReader(Paths.get(new URI(filePathString)));
+            String filePathString;
+            if (args.length > 0) {
+                filePathString = args[0];
+            } else {
+                Scanner s = new Scanner(System.in);
+                System.out.println("Please enter the full file path to compile: (Relative to your home directory)");
+                filePathString = s.nextLine();
+            }
+
+            filePathString = "/Users/" + System.getProperty("user.name") + "/" + filePathString;
+            File file = new File(filePathString);
+            if (!file.exists()) {
+                System.out.println("No file found at the location:");
+                System.out.println(file.getAbsolutePath());
+                System.exit(0);
+            }
+            ArrayList<String> path = new ArrayList<>(Arrays.asList(filePathString.split("/")));
+            path.remove(path.size() - 1);
+            path.add(file.getName().replaceAll("\\..*", ".java"));
+            StringBuilder writeFilePathString = new StringBuilder();
+            path.forEach(l -> {
+                writeFilePathString.append(l).append("/");
+            });
+            writeFilePathString.reverse().deleteCharAt(0).reverse();
+            System.out.println("Writing to " + writeFilePathString.toString() + "\n");
+            w = new PrintWriter(writeFilePathString.toString(), "UTF-8");
+            BufferedReader b = new BufferedReader(new FileReader(file));
             b.lines().forEach(line -> {
                 StormParser.parseLine(line);
+                System.out.println("Parsing " + line);
             });
+            w.close();
+            System.out.println("\nDone");
         } catch (Exception e) {
             System.out.println("Error");
             System.out.println();
@@ -29,6 +52,10 @@ public class Main {
     }
 
     public static void writeLine(String line) {
-        writeFile = new File();
+        try {
+            w.println(line);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/com/samgauck/StormParser.java
+++ src/com/samgauck/StormParser.java
@@ -2,6 +2,10 @@ package com.samgauck;
 
 public class StormParser {
     public static void parseLine(String line) {
+        line += "\twitten bi me"; //for now, just show it was accessed
 
+        //Actually write the line
+        Main.writeLine(line);
+        return;
     }
 }
